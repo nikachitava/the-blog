08682bd3f56d1bf5946636519bce130129c757bd
nikachitava <chitava18.official@gmail.com>
2024-07-04 14:34:22 +0400

    little update in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
 	return (
 		<div className="h-20 py-5">
-			<ul className="flex items-center gap-4 text-center">
+			<ul className="md:flex items-center gap-4 text-center">
 				<li>(c) 2023</li>
 				<li>Github</li>
 				<li>LinkedIn</li>
